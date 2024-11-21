9c93faffc453c87254cb2e244d1bd9b2a4ac47b5
Nagalakshman B S <136301508+Nagalakshman410@users.noreply.github.com>
2024-11-21 22:36:53 +0530

    Update Listings.jsx

--- client/src/components/Listings.jsx
+++ client/src/components/Listings.jsx
@@ -18,8 +18,8 @@ const Listings = () => {
     try {
       const response = await fetch(
         selectedCategory !== "All"
-          ? `http://localhost:3001/properties?category=${selectedCategory}`
-          : "http://localhost:3001/properties",
+          ? `https://dreamnest-project.onrender.com/properties?category=${selectedCategory}`
+          : "https://dreamnest-project.onrender.com/properties",
         {
           method: "GET",
         }
